b577abd7b11d0b31e4b496b446490f75e814c8f6
Adam Tse <mr.adam.tse@gmail.com>
2017-02-27 03:49:13 -0500

    doc update: fix the barcode scanner

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
 
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
+cordova:any;
+  constructor(public nav: NavController, private platform: Platform, public alert: AlertController) {
 
   }
 
+  scan() {
+      this.platform.ready().then(() => {
+        BarcodeScanner.scan().then((barcodeData)=>{
+          this.alert.create({
+                  title: "Scan Results",
+                  subTitle: barcodeData.text,
+                  buttons: ["Close"]
+              }).present()
+          }, (error) => {
+          this.alert.create({
+                  title: "Attention!",
+                  subTitle: error,
+                  buttons: ["Close"]
+              }).present();
+          });
+      });
+
+}
 }
